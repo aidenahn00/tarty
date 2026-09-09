7b095fbf345355c13e9b57e9d99a6efdcfce521f
aidenahn00 <aidenahn00@gmail.com>
2026-09-10 00:24:02 +0900

    베스트 메뉴 섹션 디자인 에러 수정, 팝업 버튼 누를 시에만 보이도록 수정

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,15 @@
 const popupClose = document.querySelectorAll('.popup-close-script');
 const popup = document.querySelector('.popup');
 const modal = document.querySelector('.modal');
+const benefitBar = document.querySelector('.benefit-bar');
 popupClose.forEach((el)=>{
     el.addEventListener('click', ()=>{
         popup.style.display = 'none';
         modal.style.display = 'none';
     }); 
+});
+benefitBar.addEventListener('click', el=>{
+    el.preventDefault();
+    popup.style.display = 'block';
+    modal.style.display = 'block';
 });
